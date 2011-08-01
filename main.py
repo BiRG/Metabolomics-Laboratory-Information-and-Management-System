1149c09f7c471c4a4b68e3105b6627fdb8054e12
Dan C. Wlodarski <dan.wlodarski@gmail.com>
2011-08-01 03:52:06 -0400

    - Hooks for deployment testing.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# Based on Google's official Python documentation
+import cgi
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -8,15 +10,56 @@ class MainHandler(webapp.RequestHandler):
         user = users.get_current_user()
 
         if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, ' + user.nickname())
+            self.response.out.write("""<html>
+    <body>
+        <form action="/upload" method="post">
+            <div><input type="submit" value="Upload the Selected File"></div>
+        </form>
+        <form action="/download" method="post">
+            <div><input type="submit" value="Download a File"></div>
+        </form>
+    </body>
+</html>
+""")
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
+class DownloadHandler(webapp.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+
+        if user:
+            self.response.out.write("""<!DOCTYPE HTML>
+<html>
+    <body>
+        <a href="">Your file is ready.</a>
+    </body>
+</html>
+""")
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+class UploadHandler(webapp.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+
+        if user:
+            self.response.out.write("""<!DOCTYPE HTML>
+<html>
+    <body>
+        <p>Your file is uploaded.</p>
+    </body>
+</html>
+""")
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
-    util.run_wsgi_app(application)
+    application = webapp.WSGIApplication([('/', MainHandler),
+                                          ('/upload', UploadHandler),
+                                          ('/download', DownloadHandler)],
+                                      debug=True)
+    run_wsgi_app(application)
 
 
 if __name__ == '__main__':
