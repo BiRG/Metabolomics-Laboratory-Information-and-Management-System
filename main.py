8f6b7ecbd51853574847f9d13894f3f992e9f73d
Dan C. Wlodarski <dan.wlodarski@gmail.com>
2011-08-30 20:00:12 -0400

    - Fixed some directory targeting issues - Corrected an OAuth bug (wrong key/secret combination)

--- main.py
+++ main.py
@@ -15,15 +15,22 @@ import glims
 glims = reload(glims)
 
 
-class OAuthSetup:
-    def __init__(self,value):
-        SETTINGS = {
-        'APP_NAME': 'birgdata',
-        'CONSUMER_KEY': 'tempKeyNotGood',
-        'CONSUMER_SECRET': 'MuchLongerBetterKeyNeededSecurely',
-        'SCOPES': ['https://docs.google.com/feeds/']
-        }
-        gdocs = gdata.docs.client.DocsClient(source = SETTINGS['APP_NAME'])
+# From http://code.google.com/appengine/articles/python/retrieving_gdata_feeds.html
+# Constants included for ease of understanding. It is a more common
+# and reliable practice to create a helper for reading a Consumer Key
+# and Secret from a config file. You may have different consumer keys
+# and secrets for different environments, and you also may not want to
+# check these values into your source code repository.
+SETTINGS = {
+    'APP_NAME': 'birgdata',
+    'CONSUMER_KEY': '',
+    'CONSUMER_SECRET': '',
+    'SCOPES': ['https://docs.google.com/feeds/']
+    }
+
+# From http://code.google.com/appengine/articles/python/retrieving_gdata_feeds.html
+# Create an instance of the DocsService to make API calls
+gdocs = gdata.docs.client.DocsClient(source = SETTINGS['APP_NAME'])
 
 
 class NoCollectionWithIDException(Exception):
@@ -148,14 +155,11 @@ class FileSendHandler(webapp.RequestHandler):
             self.redirect("google.com")
 
 
-class MainHandler(webapp.RequestHandler):
+class FrontendHandler(webapp.RequestHandler):
 
     @login_required
     def get(self):
-        user = users.get_current_user()
-
-        if user:
-            self.response.out.write("""<!DOCTYPE HTML>
+        self.response.out.write("""<!DOCTYPE HTML>
 <html>
     <head>
         <title></title>
@@ -173,8 +177,97 @@ class MainHandler(webapp.RequestHandler):
     </body>
 </html>
 """)
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+
+
+# Based on Fetcher class from
+# http://code.google.com/appengine/articles/python/retrieving_gdata_feeds.html
+class MainHandler(webapp.RequestHandler):
+
+    @login_required
+    def get(self):
+        """This handler is responsible for fetching an initial OAuth
+        request token and redirecting the user to the approval page."""
+
+        current_user = users.get_current_user()
+
+        # We need to first get a unique token for the user to
+        # promote.
+        #
+        # We provide the callback URL. This is where we want the
+        # user to be sent after they have granted us
+        # access. Sometimes, developers generate different URLs
+        # based on the environment. You want to set this value to
+        # "http://localhost:8080/step2" if you are running the
+        # development server locally.
+        #
+        # We also provide the data scope(s). In general, we want
+        # to limit the scope as much as possible. For this
+        # example, we just ask for access to all feeds.
+        scopes = SETTINGS['SCOPES']
+        oauth_callback = 'http://%s/staging' % self.request.host
+        consumer_key = SETTINGS['CONSUMER_KEY']
+        consumer_secret = SETTINGS['CONSUMER_SECRET']
+        request_token = gdocs.get_oauth_token(scopes, oauth_callback,
+                                              consumer_key, consumer_secret)
+
+        # Persist this token in the datastore.
+        request_token_key = 'request_token_%s' % current_user.user_id()
+        gdata.gauth.ae_save(request_token, request_token_key)
+
+        # Generate the authorization URL.
+        approval_page_url = request_token.generate_authorization_url()
+
+        message = """<a href="%s">
+Request token for the Google Documents Scope</a>"""
+        self.response.out.write(message % approval_page_url)
+
+
+# Based on RequestTokenCallback class from
+# http://code.google.com/appengine/articles/python/retrieving_gdata_feeds.html
+class OAuthStagingHandler(webapp.RequestHandler):
+
+    @login_required
+    def get(self):
+        """When the user grants access, they are redirected back to this
+        handler where their authorized request token is exchanged for a
+        long-lived access token."""
+
+        current_user = users.get_current_user()
+
+        # Remember the token that we stashed? Let's get it back from
+        # datastore now and adds information to allow it to become an
+        # access token.
+        request_token_key = 'request_token_%s' % current_user.user_id()
+        request_token = gdata.gauth.ae_load(request_token_key)
+        gdata.gauth.authorize_request_token(request_token, self.request.uri)
+
+        # We can now upgrade our authorized token to a long-lived
+        # access token by associating it with gdocs client, and
+        # calling the get_access_token method.
+        gdocs.auth_token = gdocs.get_access_token(request_token)
+
+        # Note that we want to keep the access token around, as it
+        # will be valid for all API calls in the future until a user
+        # revokes our access. For example, it could be populated later
+        # from reading from the datastore or some other persistence
+        # mechanism.
+        access_token_key = 'access_token_%s' % current_user.user_id()
+        gdata.gauth.ae_save(request_token, access_token_key)
+
+        # -- DCW: Redirect the user to the main menu.
+        self.response.out.write("""<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Google Authentication Complete</title>
+        <meta http-equiv="refresh" content="5;url=/mainmenu" />
+    </head>
+    <body>
+        <p>Authentication with Google servers was successful!<br />
+            <a href="/mainmenu">Click this link if you are not redirected in 5 seconds.</a>
+        </p>
+    </body>
+</html>
+""")
 
 
 class TransferHandler(webapp.RequestHandler):
@@ -250,6 +343,8 @@ class UploadHandler(webapp.RequestHandler):
 
 def main():
     application = webapp.WSGIApplication([('/', MainHandler),
+                                          ('/staging', OAuthStagingHandler),
+                                          ('/mainmenu', FrontendHandler),
                                           ('/upload', UploadHandler),
                                           ('/download', DownloadHandler),
                                           ('/xfer', TransferHandler),
